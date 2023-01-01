98192b5ee4537f9ecfba37ffa8aa4704498653fb
Fran Zekan <zekan.fran369@gmail.com>
2023-01-01 16:47:57 +0100

    Finish day17

--- day-17/main.py
+++ day-17/main.py
@@ -1,23 +1,33 @@
 import sys
-import itertools
 
 lines = sys.stdin.readlines()
-data = [int(line.strip()) for line in lines]
+data = [int(line.strip()) for line in lines[1:]]
+N = int(lines[0][1:])
 
 
-def sums_up_to(data: list[int], target: int):
+def sums_up_to(data: list[int], target: int, path: list[int] = []):
     count = 0
+    paths = []
+
     for i in range(len(data)):
         if target < data[i]:
             continue
 
         if target == data[i]:
+            paths.append(path + [data[i]])
             count += 1
             continue
 
-        count += sums_up_to(data[i+1:], target - data[i])
+        tc, tp = sums_up_to(data[i+1:], target - data[i], path + [data[i]])
 
-    return count
+        count += tc
+        paths.extend(tp)
+
+    return [count, paths]
 
 
-print(sums_up_to(data, 150))
+num, paths = sums_up_to(data, N)
+shortest = min([len(path) for path in paths])
+
+print(num)
+print(len(list(filter(lambda path: len(path) == shortest, paths))))
